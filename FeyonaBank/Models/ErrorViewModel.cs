d93a14da2c3d1af8c953eaca920b5f90ad0ee79e
Feyona N. Thylander <Feyona.NaluzziThylander@yh.nackademin.se>
2018-11-07 13:34:40 +0100

    Add project files.

---
+++ FeyonaBank/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FeyonaBank.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
