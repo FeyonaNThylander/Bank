c638de44d8a90ef78e2cd31424822ce77520ac05
Feyona N. Thylander <Feyona.NaluzziThylander@yh.nackademin.se>
2018-11-21 00:39:24 +0100

    Add Deposit, Withdraw and Unittests

---
+++ FeyonaBank/Controllers/TransactionsController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FeyonaBank.Interfaces;
+using FeyonaBank.Models;
+using FeyonaBank.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeyonaBank.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private IBankRepository _bankRepo;
+
+        public TransactionsController(IBankRepository bankRepo)
+        {
+            _bankRepo = bankRepo;
+        }
+
+        public IActionResult Verification()
+        {
+            var viewmodel = new TransactionsViewModel();
+            return View(viewmodel);
+        }
+
+        public IActionResult DepositAndWithdraw()
+        {
+            var viewmodel = new TransactionsViewModel();
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public IActionResult Deposit(TransactionsViewModel model)
+        {
+            var customerList = _bankRepo.GetAllCustomers().SelectMany(y => y.BankAccounts);
+            var customerAccount = customerList.Where(x => x.AccountId == model.BankAccount.AccountId).SingleOrDefault();
+
+            if (customerAccount == null)
+            {
+                ViewBag.Message = "The accountnumber is invalid, please enter another number.";
+                return View("DepositAndWithdraw", model);
+            }
+
+            if (model.Amount == 0)
+            {
+                ViewBag.Message = "Please enter an amount higher than 0 SEK.";
+                return View("DepositAndWithdraw", model);
+            }
+
+            if (model.Amount > 0)
+            {
+                _bankRepo.DepositAmount(model.Amount, customerAccount);
+                model.BankAccount.Balance = customerAccount.Balance;
+
+                ViewBag.Message = "You have successfully made a deposit!";
+                return View("Verification", model);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Withdraw(TransactionsViewModel model)
+        {
+            var customerList = _bankRepo.GetAllCustomers().SelectMany(y => y.BankAccounts);
+            var customerAccount = customerList.Where(x => x.AccountId == model.BankAccount.AccountId).SingleOrDefault();
+
+            if (customerAccount == null)
+            {
+                ViewBag.Message = "The accountnumber is incorrect, please enter another number.";
+                return View("DepositAndWithdraw", model);
+            }
+
+            if (model.Amount == 0)
+            {
+                ViewBag.Message = "Please enter an amount higher than 0 SEK.";
+                return View("DepositAndWithdraw", model);
+            }
+
+            if (customerAccount.Balance <= model.Amount)
+            {
+                ViewBag.Message = "Your amount is more than the total balance of the account, please enter a new amount.";
+                return View("DepositAndWithdraw", model);
+            }
+            else
+            {
+                _bankRepo.WithdrawAmount(model.Amount, customerAccount);
+                model.BankAccount.Balance = customerAccount.Balance;
+
+                ViewBag.Message = "You have successfully made a withdrawl!";
+                return View("Verification", model);
+            }
+
+        }
+
+    }
+}
--- FeyonaBank/Repository/BankRepository.cs
+++ FeyonaBank/Repository/BankRepository.cs
@@ -11,89 +11,109 @@ namespace FeyonaBank.Models
     public class BankRepository : IBankRepository
     {
 
-        List<Customer> customerList = new List<Customer>();
-
+        private static List<Customer> CustomerList;
 
         public List<Customer> GetAllCustomers()
         {
-            customerList.Add(new Customer()
+            if (CustomerList == null)
             {
-                Id = 1005,
-                OrgNumber = "559268-7528",
-                CompanyName = "ICA Bomben",
-                Address = "Hökarängsvägen 8",
-                ZipCode = "S-12356",
-                City = "Farsta",
-                Country = "Sweden",
-                Telephone = "070-46569308",
-                BankAccounts = new List<Account>()
+                CustomerList = new List<Customer>();
+
+                CustomerList.Add(new Customer()
+                {
+                    Id = 1005,
+                    OrgNumber = "559268-7528",
+                    CompanyName = "ICA Bomben",
+                    Address = "Hökarängsvägen 8",
+                    ZipCode = "S-12356",
+                    City = "Farsta",
+                    Country = "Sweden",
+                    Telephone = "08-4656930",
+                    BankAccounts = new List<Account>()
                 {
                     new Account
                     {
-                        Id = 1005,
-                        AccountNumber = 13019,
-                        Balance = 148800
+                        AccountId = 13019,
+                        Balance = 15000
                     },
                     new Account
                     {
-                        Id = 1005,
-                        AccountNumber = 13020,
-                        Balance = 613603
+                        AccountId = 13020,
+                        Balance = 60000
                     }
 
                 }
-            });
+                });
 
-            customerList.Add(new Customer()
-            {
-                Id = 1024,
-                OrgNumber = "456392-8406",
-                CompanyName = "Bagarn o Konditorin",
-                Address = "Vallhallavägen 24",
-                ZipCode = "S-12357",
-                City = "Stockholm",
-                Country = "Sweden",
-                Telephone = "073-7503765",
-                BankAccounts = new List<Account>()
+                CustomerList.Add(new Customer()
+                {
+                    Id = 1024,
+                    OrgNumber = "456392-8406",
+                    CompanyName = "Bagarn o Konditorin",
+                    Address = "Vallhallavägen 24",
+                    ZipCode = "S-12357",
+                    City = "Stockholm",
+                    Country = "Sweden",
+                    Telephone = "073-7503765",
+                    BankAccounts = new List<Account>()
                 {
                     new Account
                     {
-                        Id = 1024,
-                        AccountNumber = 13093,
+                        AccountId = 13093,
                         Balance = 69558
                     },
                 }
-            });
+                });
 
-            customerList.Add(new Customer()
-            {
-                Id = 1032,
-                OrgNumber = "571553-1910",
-                CompanyName = "Studio Mo Leah",
-                Address = "Vaniljvägen 100",
-                ZipCode = "S-957602",
-                City = "Göteborg",
-                Country = "Sweden",
-                Telephone = "070-44876890",
-                BankAccounts = new List<Account>()
+                CustomerList.Add(new Customer()
+                {
+                    Id = 1032,
+                    OrgNumber = "571553-1910",
+                    CompanyName = "Studio Mo Leah",
+                    Address = "Vaniljvägen 100",
+                    ZipCode = "S-957602",
+                    City = "Göteborg",
+                    Country = "Sweden",
+                    Telephone = "070-44876890",
+                    BankAccounts = new List<Account>()
                 {
                     new Account
                     {
-                        Id = 1032,
-                        AccountNumber = 13128,
+                        AccountId = 13128,
                         Balance = 39200
                     },
                      new Account
                     {
-                        Id = 1032,
-                        AccountNumber = 13130,
+                        AccountId = 13130,
                         Balance = 48070
                     }
                 }
-            });
+                });
+            }
 
-            return customerList;
+            return CustomerList;
         }
+
+        public bool DepositAmount(int amount, Account bankAccount)
+        {
+            bankAccount.Balance += amount;
+
+            return true;
+        }
+
+        public bool WithdrawAmount(int amount, Account bankAccount)
+        {
+            if (bankAccount.Balance < amount)
+            {
+                return false;
+            }
+            else
+            {
+                bankAccount.Balance -= amount;
+                return true;
+            }
+        }
+
 
     }
 }
